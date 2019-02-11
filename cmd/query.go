3c706cba2308ca8c2c1fca44ab07913ee2ba85c2
Jaehong Ahn <monsieur.ahn@gmail.com>
2019-02-11 22:38:13 +0900

    Add address info querying feature

--- cmd/query.go
+++ cmd/query.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+
+	atypes "github.com/amolabs/amoabci/amo/types"
+	"github.com/amolabs/amoconsole/tx"
 )
 
 /* Commands (expected hierarchy)
@@ -22,6 +27,26 @@ var queryCmd = &cobra.Command{
 	},
 }
 
+var queryAddressCmd = &cobra.Command{
+	Use:   "address [address]",
+	Short: "Shows address's general information",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		target := atypes.NewAddressFromBytes([]byte(args[0]))
+		targetInfo, err := tx.QueryAddressInfo(*target)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(targetInfo))
+
+		return nil
+	},
+}
+
 func init() {
 	// init here if needed
+	addressCmd := queryAddressCmd
+	cmd := queryCmd
+	cmd.AddCommand(addressCmd)
 }
--- tx/query.go
+++ tx/query.go
@@ -1,3 +1,16 @@
 package tx
 
-// Query functions
+import (
+	atypes "github.com/amolabs/amoabci/amo/types"
+	"github.com/amolabs/amoconsole/util"
+)
+
+// QueryAddressInfo is ..
+func QueryAddressInfo(target atypes.Address) ([]byte, error) {
+	result, err := util.RPCABCIQuery("", target[:])
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Response.Value, nil
+}
